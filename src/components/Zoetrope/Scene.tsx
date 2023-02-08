10c617518005c6191663a25f032ff5cd5f2b8e91
Sami <melahi@uchicago.edu>
2023-02-08 11:02:52 -0800

    set up loading of strips on zoetrope menu

--- src/components/Zoetrope/Scene.tsx
+++ src/components/Zoetrope/Scene.tsx
@@ -2,14 +2,36 @@ import Zoetrope from "./Zoetrope";
 import Table from "./Table";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { useControls } from "leva";
 import jessica from "../../assets/example_strips/Jessica.png";
+import { useState } from "react";
+import { AnimationStateDB } from "../../types";
+import { getAllAnimations } from "../../state/idb";
 
 export default function Scene() {
+  const [animations, setAnimations] = useState<Record<string, string>>({});
+
+  useEffect(() => {
+    async function loadAnimations() {
+      const loadedAnimations = await getAllAnimations();
+      const animationImageURLs: Record<string, string> = {};
+      loadedAnimations.forEach((animation) => {
+        const name = animation.name!;
+        const objectURL = URL.createObjectURL(animation.build!);
+
+        animationImageURLs[name] = objectURL;
+      });
+
+      setAnimations(animationImageURLs);
+    }
+
+    loadAnimations();
+  }, []);
+
   const { Speed, Strip } = useControls({
     Speed: { value: 0, min: 0, max: 0.05 },
-    Strip: { options: { Jessica: jessica } },
+    Strip: { options: { Jessica: jessica, ...animations } },
   });
 
   return (
