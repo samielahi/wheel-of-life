22e2c120fc26a20aa75d06a26f99c0fbe2fae8af
Sami <melahi@uchicago.edu>
2023-02-23 10:08:19 -0800

    Removed artificial delay from zoetrope page, and fixed zoom in issue

--- src/components/Zoetrope/Scene.tsx
+++ src/components/Zoetrope/Scene.tsx
@@ -50,69 +50,69 @@ export default function Scene() {
 
   return (
     <>
-        <Header type="zoetrope"></Header>
+      <Header type="zoetrope"></Header>
 
-        <div className="absolute left-[5%] top-[70%] z-20 hidden w-[16rem] rounded-md bg-yellow/40 p-6 backdrop-blur lg:block">
-          <div className="flex flex-col justify-center gap-2">
-            <h3 className="text-center text-lg text-red">Controls:</h3>
-            <p className="italic">
-              <span className="font-bold">Zoom:</span> scroll
-            </p>
-            <p className="italic">
-              <span className="font-bold">Tilt:</span> left click + drag.
-            </p>
-            <p className="italic">
-              <span className="font-bold">Pan:</span> right click + drag.
-            </p>
-          </div>
+      <div className="absolute left-[5%] top-[70%] z-20 hidden w-[16rem] rounded-md bg-yellow/40 p-6 backdrop-blur lg:block">
+        <div className="flex flex-col justify-center gap-2">
+          <h3 className="text-center text-lg text-red">Controls:</h3>
+          <p className="italic">
+            <span className="font-bold">Zoom:</span> scroll
+          </p>
+          <p className="italic">
+            <span className="font-bold">Tilt:</span> left click + drag.
+          </p>
+          <p className="italic">
+            <span className="font-bold">Pan:</span> right click + drag.
+          </p>
         </div>
+      </div>
 
-        <Controls
-          changeSpeed={changeZoetropeSpinSpeed}
-          selectStrip={selectStrip}
-          animations={animations}
+      <Controls
+        changeSpeed={changeZoetropeSpinSpeed}
+        selectStrip={selectStrip}
+        animations={animations}
+      />
+
+      <Canvas
+        shadows
+        camera={{ position: [0, 2.75, 15], fov: 60 }}
+        className="cursor-grab overflow-hidden"
+      >
+        <OrbitControls
+          maxDistance={15}
+          minDistance={0}
+          minAzimuthAngle={0}
+          maxAzimuthAngle={0}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 2.5}
         />
-
-        <Canvas
-          shadows
-          camera={{ position: [0, 2.75, 15], fov: 60 }}
-          className="cursor-grab overflow-hidden"
+        <Stage
+          adjustCamera={false}
+          intensity={0.4}
+          shadows={{
+            type: "accumulative",
+            color: "gray",
+            colorBlend: 1,
+            opacity: 0.4,
+          }}
+          // @ts-ignore
+          environment={null}
+          preset="rembrandt"
         >
-          <OrbitControls
-            maxDistance={15}
-            minDistance={5}
-            minAzimuthAngle={0}
-            maxAzimuthAngle={0}
-            minPolarAngle={0}
-            maxPolarAngle={Math.PI / 2.5}
+          <Environment files={"/lebombo_1k.hdr"} />
+
+          <Strip></Strip>
+          <Zoetrope
+            onClick={() => setZoomed(!zoomed)}
+            ref={zoetropeRef}
+            speed={speed}
+            image={currentStrip}
           />
-          <Stage
-            adjustCamera={false}
-            intensity={0.4}
-            shadows={{
-              type: "accumulative",
-              color: "gray",
-              colorBlend: 1,
-              opacity: 0.4,
-            }}
-            // @ts-ignore
-            environment={null}
-            preset="rembrandt"
-          >
-            <Environment files={"/lebombo_1k.hdr"} />
+          <Table />
+        </Stage>
+      </Canvas>
 
-            <Strip></Strip>
-            <Zoetrope
-              onClick={() => setZoomed(!zoomed)}
-              ref={zoetropeRef}
-              speed={speed}
-              image={currentStrip}
-            />
-            <Table />
-          </Stage>
-        </Canvas>
-
-        <Footer />
+      <Footer />
     </>
   );
 }
--- src/routes/zoetrope.tsx
+++ src/routes/zoetrope.tsx
@@ -1,9 +1,8 @@
 import { lazy } from "react";
-import { artificialDelay } from "../utils";
 import LoadingSpinner from "../core/LoadingSpinner";
 import { Suspense } from "react";
 
-const Scene = lazy(() => artificialDelay(import("../components/Zoetrope/Scene")));
+const Scene = lazy(() => import("../components/Zoetrope/Scene"));
 
 export default function Zoetrope() {
   return (
